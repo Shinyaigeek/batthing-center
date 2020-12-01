6bece2e6df9d76c9d4663c8f44b4b9cae676f303
shinyaigeek <PhilisPaxil@gmail.com>
2020-12-02 03:32:57 +0900

    [pattern-info]add: data input

---
+++ pattern-info/input/input.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def input_data_csv(csv, idx_col):
+    return pd.read_csv("data/" + csv, index_col=idx_col)
+
+def input_data_np(csv, idx_col, factors):
+    csv_data = input_data_csv(csv, idx_col)
+    return pd.DataFrame(csv_data[factors].copy()).to_numpy();
